95461b102a932ab232fcc40a77cec19535d40fe9
MuhammadBabarAbbas <abbasmuhammadbabar@gmail.com>
2022-08-07 12:58:18 +0500

    Adding Routing to the Project

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <p>
+                <Link to={`/product/${product.slug}`}>{product.name}</Link>
+              </p>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
